7b76184459bd10461ec31a25e3464a274c1b839d
Rittwik <82934269+rikde1999@users.noreply.github.com>
2021-09-12 21:24:56 +0530

    Delete Golf2 directory

--- Golf2/obstacle.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Obstacles(pygame.sprite.Sprite):
-    def __init__(self,picture_path,x_pos,y_pos):
-        super().__init__()
-        self.image = pygame.image.load(picture_path)
-        self.rect = self.image.get_rect(center = (x_pos,y_pos))
--- Golf2/settings.py
+++
@@ -1,1 +0,0 @@
-screen_width,screen_height = 300,450
